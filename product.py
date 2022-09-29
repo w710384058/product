9a28753ec2d47ac6082d56be8a600baf6f69031f
kimmy <w710384058@gmail.com>
2022-09-29 17:38:06 +0800

    add read file version 2

--- product.py
+++ product.py
@@ -1,16 +1,19 @@
 import os #operating system
 
-if os.path.isfile('products.csv')
-#讀取檔案
-products=[]
-with open ('products.csv','r',encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue #繼續跳到下一迴
-		name, price = line.strip().split(',')
-		products.append([name,price])
 
-print(products)
+products=[]
+if os.path.isfile('products.csv'):#檢查檔案在不在
+	print('yeah!找到檔案了!')
+	#讀取檔案
+	with open ('products.csv','r',encoding='utf-8') as f:
+		for line in f:
+			if '商品,價格' in line:
+				continue #繼續跳到下一迴
+			name, price = line.strip().split(',')
+			products.append([name,price])
+	print(products)
+else:
+	print('找不到檔案......')
 
 #讓使用者輸入
 while True:
